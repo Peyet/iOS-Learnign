ea40359aa3080ab70d6a63c8a7b64f3d21d0645a
peyet <1014720886@qq.com>
2021-08-30 17:23:06 +0800

    ApplicationDownloader

---
+++ Objective-C/Day03-ApplicationDownloader/Day03-ApplicationDownloader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Day03-ApplicationDownloader
+//
+//  Created by Peyet-Zhao on 2021/8/30.
+//  Copyright © 2021 Peyet-Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
