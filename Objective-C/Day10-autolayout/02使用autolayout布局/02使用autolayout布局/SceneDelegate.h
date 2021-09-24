4675a879542b717629c6a13e14d6e66c4c249aad
peyet <1014720886@qq.com>
2021-09-24 15:24:08 +0800

    autolayout

---
+++ Objective-C/Day10-autolayout/02使用autolayout布局/02使用autolayout布局/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  02使用autolayout布局
+//
+//  Created by Peyet-Zhao on 2021/9/24.
+//  Copyright © 2021 Peyet-Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Objective-C/Day10-autolayout/02使用autolayout布局/02使用autolayout布局/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  02使用autolayout布局
+//
+//  Created by Peyet-Zhao on 2021/9/24.
+//  Copyright © 2021 Peyet-Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
