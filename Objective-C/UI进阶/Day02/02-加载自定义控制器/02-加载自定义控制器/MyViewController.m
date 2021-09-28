a13fc20609c6452f85227920a5e61632ac8a90a1
peyet <1014720886@qq.com>
2021-09-28 19:13:12 +0800

    加载自定义控制器

---
+++ Objective-C/UI进阶/Day02/02-加载自定义控制器/02-加载自定义控制器/MyViewController.m
@@ -0,0 +1,32 @@
+//
+//  MyViewController.m
+//  02-加载自定义控制器
+//
+//  Created by Peyet-Zhao on 2021/9/28.
+//  Copyright © 2021 Peyet-Zhao. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Objective-C/UI进阶/Day02/02-加载自定义控制器/02-加载自定义控制器/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  02-加载自定义控制器
+//
+//  Created by Peyet-Zhao on 2021/9/28.
+//  Copyright © 2021 Peyet-Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
