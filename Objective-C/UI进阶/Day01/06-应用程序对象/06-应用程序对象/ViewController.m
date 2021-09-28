3dce55c669c873bdd2ad71093b3a437c09c9b54c
peyet <1014720886@qq.com>
2021-09-28 17:04:51 +0800

    app启动过程

--- Objective-C/UI进阶/Day01/06-应用程序对象/06-应用程序对象/ViewController.m
+++ Objective-C/UI进阶/Day01/06-应用程序对象/06-应用程序对象/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <UserNotifications/UserNotifications.h>
 
 @interface ViewController ()
+- (IBAction)btnClicked:(UIButton *)sender;
 
 @end
 
@@ -20,4 +22,30 @@
 }
 
 
+- (IBAction)btnClicked:(UIButton *)sender {
+    /**
+            1. 应用程序对象 是 UIApplication, 是应用程序的对象 , 主要执行应用级别的操作
+            2. 应用程序对象的获取  [ UIApplication sharedApplication]
+     */
+    
+    UIApplication *app0 = [UIApplication sharedApplication];
+    UIApplication *app1 = [UIApplication sharedApplication];
+    
+    NSLog(@"app0 = %@\napp1 = %@", app0, app1);
+    
+//    app0 registerUserNotificationSettings:<#(nonnull UIUserNotificationSettings *)#>
+    UNUserNotificationCenter *userNotification = [UNUserNotificationCenter currentNotificationCenter];
+    [userNotification requestAuthorizationWithOptions:UNAuthorizationOptionBadge completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        
+    }];
+    
+
+    // 设置应用头像上的数字
+    // set to 0 to hide. default is 0. In iOS 8.0 and later, your application must register for user notifications using -[UIApplication registerUserNotificationSettings:] before being able to set the icon badge.
+    app0.applicationIconBadgeNumber = 10;
+    
+    // 设置联网状态
+    app0.networkActivityIndicatorVisible = YES;
+    
+}
 @end
---
+++ Objective-C/UI进阶/Day02/01-AppDelegate/01-AppDelegate/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  01-AppDelegate
+//
+//  Created by Peyet-Zhao on 2021/9/28.
+//  Copyright © 2021 Peyet-Zhao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
